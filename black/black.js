6749fc022cec45d97409bb7429a96cbb0f78d1f9
HeKristof <117970994+HeKristof@users.noreply.github.com>
2025-09-25 12:45:45 +0200

    penz

--- black/black.js
+++ black/black.js
@@ -71,6 +71,10 @@ startBtn.addEventListener("click", () => {
   // 🔒 Start gomb és tét letiltása
   startBtn.disabled = true;
   betInput.disabled = true;
+
+  // 🔓 Hit és Stand engedélyezése
+  hitBtn.disabled = false;
+  standBtn.disabled = false;
 });
 
 // Hit gomb
@@ -81,7 +85,7 @@ hitBtn.addEventListener("click", () => {
 
   // ha több mint 21, azonnal vége (mintha stand-et nyomtunk volna)
   if (handValue(playerHand) > 21) {
-    standBtn.click(); 
+    standBtn.click();
   }
 });
 
@@ -131,4 +135,12 @@ standBtn.addEventListener("click", () => {
   // 🔓 Start gomb és tét újra engedélyezése
   startBtn.disabled = false;
   betInput.disabled = false;
+
+  // 🔒 Hit és Stand letiltása
+  hitBtn.disabled = true;
+  standBtn.disabled = true;
 });
+
+// 🔒 Alapból tiltsuk le a Hit és Stand gombokat, hogy csak játék közben működjenek
+hitBtn.disabled = true;
+standBtn.disabled = true;
